fa44d63f0cc240cdd096463153783c16778ebce4
周飞龙 <18368404991@163.com>
2018-01-22 22:25:49 +0800

    分页实现,待解决问题:div固定宽高

--- js/content-pagination.js
+++ js/content-pagination.js
@@ -3,9 +3,10 @@
     $('input#post-search').on('change', function(event) {
       var keyword = $('input#post-search').val();
       if (keyword.length==0) {
-        $.when(
-          $('ul#archivelist li').removeClass('jp-hidden').show(400)
-        )
+        fenye(index);
+        // $.when(
+        //   $('ul#archivelist li').removeClass('hidden').show(400)
+        // )
       }else{
         $.when(
           $('ul#archivelist li').each(function(index, el) {
@@ -13,7 +14,7 @@
             if (postTitle.indexOf(keyword.toLowerCase())==-1){
               $(el).hide(400);
             } 
-            else $(el).removeClass('jp-hidden').show(400)
+            else $(el).removeClass('hidden').show(400)
           })
           )
        }
