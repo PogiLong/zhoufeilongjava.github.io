a37942d26adec23da9a1a824817827c7d0600274
周飞龙 <18368404991@163.com>
2018-02-03 19:07:12 +0800

    gitalk

--- js/gitalk.js
+++ js/gitalk.js
@@ -1,7 +1,7 @@
 const gitalk = new Gitalk({
   clientID: '4d1bfc260f0d92c3da77',
   clientSecret: 'e8cf2b04f77c50702f9fa2a399440f91d788a02b',
-  repo: 'blogTalk',
+  repo: 'zhoufeilongjava.github.io',
   owner: 'zhoufeilongjava',
   admin: ['zhoufeilongjava'],
   // facebook-like distraction free mode
