677866f9f32c9d87b7dcbb91fa5421483ba28c71
周飞龙 <18368404991@163.com>
2018-02-08 16:14:49 +0800

    update

--- js/categories.js
+++ js/categories.js
@@ -2,7 +2,6 @@
 (function () {
   function categories(){
     $('li#categories').on('click', function(event) {
-      console.log(11111);
       $.when(
           $('.sidebar-nav').append('')
       )
